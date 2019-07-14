cc069cd177994b22186f3a2394aab227c0383969
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2019-07-14 07:30:59 +0700

    Clean up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,2 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'products/show'
-  root 'products#index'
-  resources :products, only: [:index, :show]
-
-  namespace :admin do
-    root 'products#index'
-    resources :products, only: [:new, :create, :index, :edit, :update, :destroy]
-  end
-
-  resource :carts, only: [:show] do
-    post ':product_id', to: 'carts#add_item', as: :add_item
-    delete ':product_id', to: 'carts#remove_item', as: :remove_item
-  end
 end
